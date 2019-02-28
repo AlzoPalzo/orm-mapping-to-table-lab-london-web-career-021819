58c6f127ec9e1a07a9a75359e911e1067be46a75
github username <github email address>
2019-02-28 17:44:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,19 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  attr_reader :name, :grade
   
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    query = <<_ SQL
+      CREATE TABLE students
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        SQL 
+
+    DB[:conn].execute(sql)
+  end
 end
