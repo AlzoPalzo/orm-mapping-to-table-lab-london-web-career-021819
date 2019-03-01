03a6daa1cd9939f5b4956cd65c522970a33584a8
github username <github email address>
2019-03-01 08:44:27 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,19 +1,51 @@
+require 'sqlite3'
+
 class Student
-  attr_reader :name, :grade
+  attr_reader :name, :grade, :id
   
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
+    @id = id
   end
 
   def self.create_table
-    query = <<_ SQL
-      CREATE TABLE students
+    query = <<-SQL
+      CREATE TABLE students(
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
-        SQL 
+        )
+        SQL
+        
+    DB[:conn].execute(query)
+  end
 
-    DB[:conn].execute(sql)
+  def self.drop_table
+    drop = <<-SQL
+    DROP TABLE IF EXISTS students
+    SQL
+    DB[:conn].execute(drop)
+  end
+
+  def save
+    push_student = <<-SQL
+    INSERT INTO students
+    (
+      name, grade
+    )
+    VALUES
+    (
+      ?,?
+    )
+    SQL
+    DB[:conn].execute(push_student, @name, @grade)
+    @id = DB[:conn].execute("SELECT students.id FROM students ORDER BY students.id DESC LIMIT 1").flatten[0]
+  end
+
+  def self.create(attributes)
+    s = Student.new(attributes[:name], attributes[:grade])
+    s.save    
+    s
   end
 end
